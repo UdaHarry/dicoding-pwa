ca7af280d212236906bf993b609645e244d1ef60
UdaHarry <harryrahmansyah97@gmail.com>
2020-05-16 12:04:22 +0700

    ES6 Styles

--- js/nav.js
+++ js/nav.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 	// SIDEBAR NAVIGATION
-	var elems = document.querySelectorAll('.sidenav');
+	const elems = document.querySelectorAll('.sidenav');
 	M.Sidenav.init(elems);
 	loadNav();
 
 	function loadNav()
 	{
-		var xhttp = new XMLHttpRequest();
+		const xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 			if (this.readyState == 4){
 				if(this.status != 200) return;
 
 				// Muat daftar tautan menu
 				document.querySelectorAll(".topnav, .sidenav")
-				.forEach(function(elm){
+				.forEach( elm =>{
 					elm.innerHTML = xhttp.responseText;
 				});
 
 				// Daftarkan event listener untuk setiap tautan menu
 				document.querySelectorAll('.sidenav a, .topnav a')
-				.forEach(function(elm){
-					elm.addEventListener('click', function(event){
+				.forEach( elm =>{
+					elm.addEventListener('click', event => {
 						// Tutup sidenav
-						var sidenav = document.querySelector('.sidenav');
+						const sidenav = document.querySelector('.sidenav');
 						M.Sidenav.getInstance(sidenav).close();
 						
 						// Muat konten halaman yang dipanggil 
@@ -38,16 +38,16 @@ document.addEventListener('DOMContentLoaded', function(){
 	}
 	
 	// Load page content
-	var page = window.location.hash.substr(1);
+	let page = window.location.hash.substr(1);
 	if(page == '') page = 'home';
 	loadPage(page);
 
 	function loadPage(page)
 	{
-		var xhttp = new XMLHttpRequest();
+		const xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 			if (this.readyState == 4){
-				var content = document.querySelector(".body-content");
+				let content = document.querySelector(".body-content");
 				if(this.status == 200) {
 					content.innerHTML = xhttp.responseText;
 				} else if(this.status == 404) {
--- service-worker.js
+++ service-worker.js
@@ -8,46 +8,51 @@ var urlsToCache = [
 	'/pages/contact.html',
 	'/css/materialize.min.css',
 	'/js/materialize.min.js',
-	'/js/script.js'
+	'/js/nav.js'
 ];
 
-self.addEventListener('install', function(event){
+// Menyimpan Aset ke Cache
+self.addEventListener('install', event =>{
 	event.waitUntil(
-		caches.open(CACHE_NAME)
-		.then(function(cache) {
-			return cache.addAll(urlsToCache);
-		})
+	  caches.open(CACHE_NAME)
+			.then( cache => {
+				return cache.addAll(urlsToCache);
+			})
 	);
 })
 
-self.addEventListener('activate', function(event){
-	event.waitUntil(
-		caches.keys()
-		.then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.map(function(cacheName){
-					if(cacheName != CACHE_NAME){	
-						console.log("ServiceWorker: cache " + cacheName + " dihapus");
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
-	);
-})
-
-self.addEventListener('fetch', function(event) {
+// Menggunakan Aset dari Cache
+self.addEventListener('fetch', event => {
 	event.respondWith(
 		caches.match(event.request, {cacheName:CACHE_NAME})
-		.then(function(response) {
-			if(response){
-				console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
-				return response;
-			}
-			
-			console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
-			return fetch(event.request);
-		})
+          .then( response => {
+            if(response){
+              console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+              return response;
+            }
+            
+            console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
+            return fetch(event.request);
+          })
 	);
 });
+
+// Menghapus Cache Lama
+self.addEventListener('activate', event =>{
+	event.waitUntil(
+		caches.keys()
+          .then( cacheNames => {
+            return Promise.all(
+              cacheNames.map( cacheName =>{
+                if(cacheName != CACHE_NAME){	
+                  console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                  return caches.delete(cacheName);
+                }
+              })
+            );
+          })
+	);
+})
+
+
 
